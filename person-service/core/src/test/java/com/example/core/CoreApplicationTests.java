41611efcfc9b1a450eccb2b5cfcf3a9e77719233
Zero-imagination <disagreed7@gmail.com>
2022-10-06 16:41:05 +0300

    added person service

---
+++ person-service/core/src/test/java/com/example/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.core;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
